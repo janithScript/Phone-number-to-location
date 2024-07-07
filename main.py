f537404c50c36ce2228051d7e5a4237f5739349a
Janith Chathuranga <127806197+janithScript@users.noreply.github.com>
2024-07-08 00:56:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ pepnumber = phonenumbers.parse(number)
 location = geocoder.description_for_number(pepnumber, "en")
 print(location)
 
+
 from phonenumbers import carrier
 service_pro = phonenumbers.parse(number)
 print(carrier.name_for_number(service_pro, "en"))
